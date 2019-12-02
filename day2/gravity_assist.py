e560278817747456e296014fbb82c0fd4cb4eff6
Rog <roger.murray@iis.se>
2019-12-02 10:35:06 +0100

    day 2 solved, needs to be prettier/smarter

---
+++ day2/gravity_assist.py
@@ -0,0 +1,240 @@
+
+
+def fix_computer(initial_state: list) -> list:
+    start = 0
+    stop = 3
+    end = len(initial_state)
+    while stop < end:
+        if initial_state[start] == 99:
+            return initial_state
+        elif initial_state[start] == 1:
+            initial_state[initial_state[stop]] = (
+                initial_state[initial_state[start + 1]]
+                + initial_state[initial_state[start + 2]]
+            )
+        elif initial_state[start] == 2:
+            initial_state[initial_state[stop]] = (
+                initial_state[initial_state[start + 1]]
+                * initial_state[initial_state[start + 2]]
+            )
+        start += 4
+        stop += 4
+        # print(f"start={start}, stop={stop}")
+    return initial_state
+
+
+def find_noun_verb(initial_state: list) -> list:
+    for i in range(99):
+        for j in range(99):
+            input = [x for x in initial_state]
+            input[1] = i
+            input[2] = j
+            output = fix_computer(input)
+            if output[0] == 19690720:
+                return output[0], output[1], output[2], 100 * output[1] + output[2]
+
+if __name__ == "__main__":
+    # with open("puzzle_input") as file:
+    #     data = file.read().split(',')
+    #     print(type(data))
+    #     print(fix_computer(data))
+
+    print(
+        find_noun_verb(
+            [
+                1,
+                0,
+                0,
+                3,
+                1,
+                1,
+                2,
+                3,
+                1,
+                3,
+                4,
+                3,
+                1,
+                5,
+                0,
+                3,
+                2,
+                1,
+                6,
+                19,
+                1,
+                5,
+                19,
+                23,
+                1,
+                23,
+                6,
+                27,
+                1,
+                5,
+                27,
+                31,
+                1,
+                31,
+                6,
+                35,
+                1,
+                9,
+                35,
+                39,
+                2,
+                10,
+                39,
+                43,
+                1,
+                43,
+                6,
+                47,
+                2,
+                6,
+                47,
+                51,
+                1,
+                5,
+                51,
+                55,
+                1,
+                55,
+                13,
+                59,
+                1,
+                59,
+                10,
+                63,
+                2,
+                10,
+                63,
+                67,
+                1,
+                9,
+                67,
+                71,
+                2,
+                6,
+                71,
+                75,
+                1,
+                5,
+                75,
+                79,
+                2,
+                79,
+                13,
+                83,
+                1,
+                83,
+                5,
+                87,
+                1,
+                87,
+                9,
+                91,
+                1,
+                5,
+                91,
+                95,
+                1,
+                5,
+                95,
+                99,
+                1,
+                99,
+                13,
+                103,
+                1,
+                10,
+                103,
+                107,
+                1,
+                107,
+                9,
+                111,
+                1,
+                6,
+                111,
+                115,
+                2,
+                115,
+                13,
+                119,
+                1,
+                10,
+                119,
+                123,
+                2,
+                123,
+                6,
+                127,
+                1,
+                5,
+                127,
+                131,
+                1,
+                5,
+                131,
+                135,
+                1,
+                135,
+                6,
+                139,
+                2,
+                139,
+                10,
+                143,
+                2,
+                143,
+                9,
+                147,
+                1,
+                147,
+                6,
+                151,
+                1,
+                151,
+                13,
+                155,
+                2,
+                155,
+                9,
+                159,
+                1,
+                6,
+                159,
+                163,
+                1,
+                5,
+                163,
+                167,
+                1,
+                5,
+                167,
+                171,
+                1,
+                10,
+                171,
+                175,
+                1,
+                13,
+                175,
+                179,
+                1,
+                179,
+                2,
+                183,
+                1,
+                9,
+                183,
+                0,
+                99,
+                2,
+                14,
+                0,
+                0,
+            ]
+        )
+    )
