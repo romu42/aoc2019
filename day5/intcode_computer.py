720849449d7bce110ff76474f1bdf99fbf197277
Rog <roger.murray@iis.se>
2019-12-05 23:58:24 +0100

    day 5 part 1 done, part 2 still to do

--- day5/intcode_computer.py
+++ day5/intcode_computer.py
@@ -28,6 +28,22 @@ def computer(memory: list) -> (list, int):
             memory, position = opcode_4(
                 memory, position
             )
+        elif opcode == 5:
+            memory, position = opcode_5(
+                memory, position, param_mode1, param_mode2, param_mode3
+            )
+        elif opcode == 6:
+            memory, position = opcode_6(
+                memory, position, param_mode1, param_mode2, param_mode3
+            )
+        elif opcode == 7:
+            memory, position = opcode_7(
+                memory, position, param_mode1, param_mode2, param_mode3
+            )
+        elif opcode == 8:
+            memory, position = opcode_8(
+                memory, position, param_mode1, param_mode2, param_mode3
+            )
 
     return memory
 
@@ -79,9 +95,10 @@ def opcode_3(
     position: int,
 ) -> (list, int):
     """
-    opcode 3 stores input to address in parameter
+    opcode 3 stores input to address in parameter ( next memory position ) and moves position forward 2
     """
-    memory[memory[position + 1]] = 1
+    # memory[memory[position + 1]] = 5
+    memory[memory[position + 1]] = 8
     return memory, position + 2
 
 
@@ -89,28 +106,151 @@ def opcode_4(
     memory: list, position: int
 ) -> (list, int):
     """
-    opcode 4 outputs value of parameter
+    opcode 4 outputs value of parameter ( next memory position ) and moves memory pointer forward 2
     """
     print(memory[memory[position + 1]])
     return memory, position + 2
 
+def opcode_5(
+    memory: list, position: int, param_mode1: int, param_mode2: int, param_mode3: int
+) -> (list, int):
+    """
+    opcode 5 jump-if-true if the fist parameter is non-zero set memory pointer to value from the second parameter or do nothing
+    returns updated memory and current position in memory
+    """
+    if param_mode1 == 0:
+        param1 = memory[memory[position + 1]]
+    else:
+        param1 = memory[position + 1]
+    if param_mode2 == 0:
+        param2 = memory[memory[position + 2]]
+    else:
+        param2 = memory[position + 2]
+    if param1 != 0:
+        position = param2
+    else:
+        position = position + 3
+
+    return memory, position
+
+def opcode_6(
+    memory: list, position: int, param_mode1: int, param_mode2: int, param_mode3: int
+) -> (list, int):
+    """
+    opcode 6 jump-if-false if the fist parameter is zero set memory pointer to value from the second parameter or do nothing
+    returns updated memory and current position in memory
+    """
+    if param_mode1 == 0:
+        param1 = memory[memory[position + 1]]
+    else:
+        param1 = memory[position + 1]
+    if param_mode2 == 0:
+        param2 = memory[memory[position + 2]]
+    else:
+        param2 = memory[position + 2]
+    if param1 == 0:
+        position = param2
+    else:
+        position = position + 3
+
+    return memory, position
+
+def opcode_7(
+    memory: list, position: int, param_mode1: int, param_mode2: int, param_mode3: int
+) -> (list, int):
+    """
+    opcode 7 less than: if the first param is less than the second param store 1 in position of third param otherwise it stores 0
+    returns updated memory and current position in memory
+    """
+    if param_mode1 == 0:
+        param1 = memory[memory[position + 1]]
+    else:
+        param1 = memory[position + 1]
+    if param_mode2 == 0:
+        param2 = memory[memory[position + 2]]
+    else:
+        param2 = memory[position + 2]
+    if param_mode3 == 0:
+        param3 = memory[memory[position + 2]]
+    else:
+        param3 = memory[position + 2]
+
+    if param1 < param2:
+        param3 = 1
+    else:
+        param3 = 0
+
+    return memory, position + 4
+
+
+def opcode_8(
+        memory: list, position: int, param_mode1: int, param_mode2: int, param_mode3: int
+) -> (list, int):
+    """
+    opcode 8 equals: if the first param equals the second param store 1 in position of third param otherwise it stores 0
+    returns updated memory and current position in memory
+    """
+    if param_mode1 == 0:
+        param1 = memory[memory[position + 1]]
+    else:
+        param1 = memory[position + 1]
+    if param_mode2 == 0:
+        param2 = memory[memory[position + 2]]
+    else:
+        param2 = memory[position + 2]
+    if param_mode3 == 0:
+        param3 = memory[memory[position + 2]]
+    else:
+        param3 = memory[position + 2]
+
+    if param1 == param2:
+        param3 = 1
+    else:
+        param3 = 0
+
+    return memory, position + 4
+
+
 def parse_instructions(instructions: int) -> tuple:
     _instructions = list(str(instructions))
+
     if len(_instructions) == 2:
         opcode = int("".join(_instructions[-2:]))
         if opcode == 99:
             return 99, 0, 0, 0
+
     if len(_instructions) == 1:
-        if _instructions[0] == '3':
+        if _instructions[0] == '1':
+            return 1, 0, 0, 0
+        elif _instructions[0] == '2':
+            return 2, 0, 0, 0
+        elif _instructions[0] == '3':
             return 3, 0, 0, 0
         elif _instructions[0] == '4':
             return 4, 0, 0, 0
+        elif _instructions[0] == '5':
+            return 5, 0, 0, 0
+        elif _instructions[0] == '6':
+            return 6, 0, 0, 0
+        elif _instructions[0] == '7':
+            return 7, 0, 0, 0
+        elif _instructions[0] == '8':
+            return 8, 0, 0, 0
+
+    if len(_instructions) == 3:
+        opcode = int("".join(_instructions[-2:]))
+        param_mode1 = int(_instructions[-3])
+        param_mode2 = 0
+        param_mode3 = 0
+        return opcode, param_mode1, param_mode2, param_mode3
+
     if len(_instructions) == 4:
         opcode = int("".join(_instructions[-2:]))
         param_mode1 = int(_instructions[-3])
         param_mode2 = int(_instructions[-4])
         param_mode3 = 0
         return opcode, param_mode1, param_mode2, param_mode3
+
     if len(_instructions) == 5:
         opcode = int("".join(_instructions[-2:]))
         param_mode1 = int(_instructions[-3])
@@ -119,12 +259,7 @@ def parse_instructions(instructions: int) -> tuple:
         return opcode, param_mode1, param_mode2, param_mode3
 
 if __name__ == "__main__":
-    with open("puzzle_input") as file:
-        data = file.read().split(',')
-        print(computer(data))
-    # memory = [1002, 4, 3, 4, 33]
-    # position1 = 0
-    # opcode, param_mode1, param_mode2, param_mode3 = parse_instructions(memory[position1])
-    # print(opcode, param_mode1, param_mode2, param_mode3)
-    # memory = [3, 0, 4, 0, 99]
-    # print(computer(memory))
+    # with open("puzzle_input") as file:
+    #     data = [int(v) for v in file.read().split(',')]
+    #     print(computer(data))
+    print(computer([3,3,1107,-1,8,3,4,3,99]))
--- tests/test_intcode_computer.py
+++ tests/test_intcode_computer.py
@@ -7,7 +7,19 @@ from day5.intcode_computer import computer
     [
         ([1002, 4, 3, 4, 33], [1002, 4, 3, 4, 99]),
         ([1101, 100, -1, 4, 0], [1101, 100, -1, 4, 99]),
-        ([3, 0, 4, 0, 99], [1, 0, 4, 0, 99]),
+        ([3, 0, 4, 0, 99], [5, 0, 4, 0, 99]),
+    ],
+)
+def test_fix_computer(initial_state, final_state):
+    assert computer(initial_state) == final_state
+
+@pytest.mark.parametrize(
+    "initial_state, final_state",
+    [
+        ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [1]),
+        ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [1]),
+        ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [1]),
+        ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [1]),
     ],
 )
 def test_fix_computer(initial_state, final_state):
