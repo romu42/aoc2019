bdd83daaccf9cbfa25b4554e082c17a915833ab9
Roger Murray <roger.murray@iis.se>
2019-12-06 16:31:07 +0100

    setup for day 6

---
+++ day6/uom.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# by rog
+
+
+if __name__ == '__main__':
+    with open('test_input') as file:
+        # data = file.read()
+        data = file.read().split('\n')
+        print(data)
