a99c0533596f2c8b01945a8d405cbce2b28325f2
Rog <roger.murray@iis.se>
2019-12-19 07:15:02 +0100

    closer?

--- day6/uom.py
+++ day6/uom.py
@@ -2,18 +2,6 @@
 # by rog
 
 from collections import defaultdict
-from tempfile import TemporaryDirectory
-import os
-
-if __name__ == "__main__":
-    with open("test_input") as file:
-        # data = file.read()
-        new_list = []
-        data = file.read().split("\n")
-        # print(data)
-        new_list = [item.split(")") for item in data]
-        # print(new_list)
-
 
 my_list = [
     ["B", "C"],
@@ -28,14 +16,6 @@ my_list = [
     ["K", "L"],
     ["COM", "B"],
 ]
-# create a direcotry
-def create_directory(pathname):
-    try:
-        os.makedirs(path)
-    except OSError:
-        print("Creation of the directory %s failed" % path)
-    else:
-        print("Successfully created the directory %s" % path)
 
 # use a defaultdict to create dict of lists where each key has the values of the
 # next orbiting bodies
@@ -49,28 +29,30 @@ print(orbits)
 # start at com and get next body
 current_body = "COM"
 next_body = orbits[current_body]
-orbit_count = ['COM']
-# create temp dir to workin
-# dir = TemporaryDirectory()
-# define the name of the directory to be created
-path = current_body
-create_directory(path)
+print(f"{current_body} -> {next_body}")
+# current_body = next_body
+orbit_count = 0
 
-# go from body to body and build the tree
-os.chdir(path)
-while next_body:
-    print(f"{current_body} -> {next_body}")
-    if len(next_body) > 1:
-        for i in range(len(next_body)):
-            path = next_body[i]
-            create_directory(path)
-    else:
-        path = next_body[0]
-        create_directory(path)
+def get_next_body(current_body, orbit_count):
+    for body in orbits[current_body]:
+        orbit_count += 1
+        if body:
+            for bod in body:
+                print(body)
+                get_next_body(bod, orbit_count)
+    return orbit_count
+
+print(get_next_body('COM', orbit_count))
 
 
-   orbit_count.append(next_body[0])
-   current_body = next_body[0]
-   next_body = orbits[current_body]
-   os.chdir(path)
-print(orbit_count)
+
+
+if __name__ == "__main__":
+    with open("test_input") as file:
+        # data = file.read()
+        new_list = []
+        data = file.read().split("\n")
+        # print(data)
+        new_list = [item.split(")") for item in data]
+        # print(new_list)
+
